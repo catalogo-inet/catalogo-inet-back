59ac19d33a08da4b1b2773e946a7e2d2f8d84e63
d4rm5 <dantemartinez06@gmail.com>
2023-10-23 12:42:00 -0300

    config db.js

--- src/db.js
+++ src/db.js
@@ -4,6 +4,6 @@ import mysql from "mysql2/promise";
 export const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "db-innet",
+  password: "root",
+  database: "dbinnet",
 });
